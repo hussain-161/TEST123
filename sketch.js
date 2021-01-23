1a6fc4150a51d0f0c38603e68d04bb58b3b8bbcc
hussain-161 <75667726+hussain-161@users.noreply.github.com>
2021-01-23 13:14:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){ 
+  
+    canvas = createCanvas(800,600);
+
+    //create 4 different surfaces
+
+    var a = createSprite(100,400,20,20)
+    a.shapeColor="RED"
+
+    //create box sprite and give velocity
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    //create edgeSprite
+
+   drawSprites();
+
+    //add condition to check if box touching surface and make it box
+}
